3cca26111d0fa07362e9c5f85138fd2014ef957d
ammu6495 <33921831+ammu6495@users.noreply.github.com>
2019-03-01 08:58:24 +0530

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoapp.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getstring()
+        {
+            return View();
+        }
+        public ActionResult getstudent()
+        {
+            return View();
+        }
+	}
+}
---
+++ Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace demoapp.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage="required")]
+    [Display(Name="student name")]
+
+        public string sname { get; set; }
+
+        [Display(Name = "student address")]
+        [DataType(DataType.MultilineText)]
+
+        [Required(ErrorMessage = "required")]
+        public string saddress { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "student email")]
+        [Required(ErrorMessage = "required")]
+
+        public string semail{ get; set; }
+        public  string  Gender { get; set; }
+        public Course CourseList { get; set; }
+
+       
+        public string username { get; set; }
+        [Display(Name = "password")]
+        [Required(ErrorMessage = "required")]
+        
+      
+        public string password { get; set; }
+
+        [Display(Name = "confirm password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "required")]
+        public string confirmpassword { get; set; }
+
+        public bool isagree { get; set; }
+    }
+   
+    public enum Course
+    {
+        MCA,MBA,Mtech,Btech
+    }
+    
+}
